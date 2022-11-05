2328af065c86b35012189100cc21e36f832ebabc
2021chena31 <82463703+2021chena31@users.noreply.github.com>
2022-11-05 17:36:38 -0500

    added wav file

--- spectro.py
+++ spectro.py
@@ -1,1 +1,14 @@
+import wave
+import nltk
 
+from nltk.corpus.reader import TimitCorpusReader
+from nltk.corpus import timit
+
+itemid = 'dr1-fvmh0/sx206'
+spkrid , sentid = itemid.split('/')
+
+item = TimitCorpusReader.utterance(spkrid, sentid)
+obj = TimitCorpusReader.wav(itemid)
+
+
+# print(TimitCorpusReader.audiodata(item))
