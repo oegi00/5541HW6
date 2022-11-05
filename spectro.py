af25459ce46293eeda23b57835df9881b53a6916
2021chena31 <82463703+2021chena31@users.noreply.github.com>
2022-11-05 18:08:42 -0500

    Wave and graphing start

--- spectro.py
+++ spectro.py
@@ -3,12 +3,46 @@ import nltk
 
 from nltk.corpus.reader import TimitCorpusReader
 from nltk.corpus import timit
+import numpy
 
-itemid = 'dr1-fvmh0/sx206'
-spkrid , sentid = itemid.split('/')
+# itemid = 'dr1-fvmh0/sx206'
+# spkrid , sentid = itemid.split('/')
 
-item = TimitCorpusReader.utterance(spkrid, sentid)
-obj = TimitCorpusReader.wav(itemid)
-
-
+# # item = TimitCorpusReader.utterance(spkrid, sentid)
+# obj = TimitCorpusReader.wav(itemid)
 # print(TimitCorpusReader.audiodata(item))
+
+sampleRate = 16000.0 #16,000 hertz sample rate
+obj = wave.open("LDC93S1.wav")
+
+# obj.setframerate(sampleRate)
+
+# print( "Number of channels",obj.getnchannels())
+# print ( "Sample width",obj.getsampwidth())
+# print ( "Frame rate.",obj.getframerate())
+# print ("Number of frames",obj.getnframes())
+# print ( "parameters:",obj.getparams())
+
+totalFrames = obj.getnframes()
+print(totalFrames)
+
+frames = obj.readframes(totalFrames)
+step = 100
+x_axis = range(0, totalFrames, step)
+y_axis = []
+
+for i in x_axis:
+    y_axis.append(frames[i])
+
+print(len(x_axis))
+print(len(y_axis))
+
+import matplotlib.pyplot as plt
+import time
+
+plt.plot(x_axis, y_axis)
+plt.show()
+#time.sleep(5)
+#plt.close()
+
+obj.close()
