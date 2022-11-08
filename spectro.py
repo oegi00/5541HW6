a3f2d3001895e6667bc2f0c4d16e03f464de131a
2021chena31 <82463703+2021chena31@users.noreply.github.com>
2022-11-08 16:02:01 -0600

    comments from OH on monday

--- spectro.py
+++ spectro.py
@@ -5,18 +5,20 @@ from nltk.corpus.reader import TimitCorpusReader
 from nltk.corpus import timit
 import numpy
 
-# itemid = 'dr1-fvmh0/sx206'
-# spkrid , sentid = itemid.split('/')
+itemid = 'dr1-fvmh0/sx206'
+spkrid , sentid = itemid.split('/')
 
-# # item = TimitCorpusReader.utterance(spkrid, sentid)
-# obj = TimitCorpusReader.wav(itemid)
-# print(TimitCorpusReader.audiodata(item))
+# item = TimitCorpusReader.utterance(spkrid, sentid)
+obj = TimitCorpusReader.wav(itemid) #does not read the object?
+print(TimitCorpusReader.audiodata(itemid))
 
 sampleRate = 16000.0 #16,000 hertz sample rate
 obj = wave.open("LDC93S1.wav")
+#find min and max values
+#turn them into floats; scale them to -1 to 1 scale
+# need to figure out what should be the 0 or negative part of the graph
 
 # obj.setframerate(sampleRate)
-
 # print( "Number of channels",obj.getnchannels())
 # print ( "Sample width",obj.getsampwidth())
 # print ( "Frame rate.",obj.getframerate())
